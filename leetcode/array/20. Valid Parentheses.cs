30200d591c27566ab09efac36dc185cf56486579
Jimmy Chang <rocmewtwo@gmail.com>
2024-07-20 12:01:14 +0800

    20. Valid Parentheses

---
+++ leetcode/array/20. Valid Parentheses.cs
@@ -0,0 +1,30 @@
+﻿var s = new _20_Valid_Parentheses();
+Console.WriteLine(s.IsValid("()")); // true
+Console.WriteLine(s.IsValid("()[]{}")); // true
+Console.WriteLine(s.IsValid("(]")); // false
+
+public class _20_Valid_Parentheses {
+    public bool IsValid(string s) {
+        var stack = new Stack<char>();
+        var mapping = new Dictionary<char, char> {
+            {')', '('},
+            {']', '['},
+            {'}', '{'}
+        };
+
+        foreach (char c in s) {
+            // If the current character is a closing bracket
+            if (mapping.ContainsKey(c)) {
+                if (stack.Count > 0 && stack.Peek() == mapping[c]) {
+                    stack.Pop();
+                } else {
+                    return false;
+                }
+            } else {
+                stack.Push(c);
+            }
+        }
+
+        return stack.Count == 0;
+    }
+}
