57694559111558bd641f16151b6585276dc33381
Jimmy Chang <rocmewtwo@gmail.com>
2024-07-20 11:41:54 +0800

    141. Linked List Cycle

---
+++ leetcode/common/ListNode.cs
@@ -0,0 +1,5 @@
+public class ListNode(int x)
+{
+    public int val = x;
+    public ListNode? next = null;
+}
---
+++ leetcode/linked_list/141. Linked List Cycle.cs
@@ -0,0 +1,20 @@
+public class _141_Linked_List_Cycle {
+    public bool HasCycle(ListNode head) {
+        ListNode? slow = head;
+        ListNode? fast = head;
+
+        while (fast != null && fast.next != null) {
+            if (slow != null)
+            {
+                slow = slow.next;
+            }
+            fast = fast.next.next;
+
+            if (slow == fast) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
